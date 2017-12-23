6d0af98530ce70cbb3523dd51bf050bada2d30c7
Francisco Muñoz <fmunozse@gmail.com>
2017-12-23 21:02:50 +0100

    Create pingMonitor.sh

---
+++ pingMonitor.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+#set -x
+user=admin
+pwd=admin
+
+url=http://192.168.1.215:8080
+pause=5
+
+
+url_auth=${url}/api/authenticate
+url_ping=${url}/api/monitor/heartbeats/ping
+
+HEADER="--header 'Content-Type: application/json' --header 'Accept: application/json' "
+JSON="{\"password\": \"${pwd}\",  \"rememberMe\": true,  \"username\": \"${user}\"}"
+PYTHON=" python -c \"import json,sys;obj=json.load(sys.stdin);print obj['id_token'];\" "
+
+while :
+do
+  cmd_auth="curl i -s -X POST $HEADER -d '$JSON' '${url_auth}' | $PYTHON "
+  eval VAR=\$\($cmd_auth\)
+
+  echo idToken=$VAR
+
+  HEADER_SECURE="$HEADER --header 'Authorization: Bearer $VAR' "
+
+  cmd_ping="curl  -i  -X POST $HEADER_SECURE '${url_ping}'  "
+  eval $cmd_ping
+
+
+  sleep $pause
+
+done
