0283425a0cdf1ce4271d318cad7f69725972d917
Francisco Muñoz <fmunozse@gmail.com>
2018-01-21 14:14:15 +0100

    Update

--- pingMonitor.sh
+++ pingMonitor.sh
@@ -4,8 +4,8 @@
 user=admin
 pwd=admin
 
-url=http://192.168.1.215:8080
-pause=5
+url=http://remotnitoring:80
+pause=300
 
 
 url_auth=${url}/api/authenticate
@@ -13,20 +13,20 @@ url_ping=${url}/api/monitor/heartbeats/ping
 
 HEADER="--header 'Content-Type: application/json' --header 'Accept: application/json' "
 JSON="{\"password\": \"${pwd}\",  \"rememberMe\": true,  \"username\": \"${user}\"}"
-PYTHON=" python -c \"import json,sys;obj=json.load(sys.stdin);print obj['id_token'];\" "
+PYTHON=" python -c \"import json,sys;obj=json.load(sys.stdin);print obj['id_token'];\" " 
 
 while :
 do
   cmd_auth="curl i -s -X POST $HEADER -d '$JSON' '${url_auth}' | $PYTHON "
   eval VAR=\$\($cmd_auth\)
 
-  echo idToken=$VAR
+  echo idToken=$VAR 
 
   HEADER_SECURE="$HEADER --header 'Authorization: Bearer $VAR' "
 
   cmd_ping="curl  -i  -X POST $HEADER_SECURE '${url_ping}'  "
   eval $cmd_ping
-
+  
 
   sleep $pause
 
